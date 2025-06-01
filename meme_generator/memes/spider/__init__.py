85fb09ec7fed12cc58154778c0e164e7f7aea74f
02 <hentai02y@gmail.com>
2025-06-01 20:28:00 +0800

    ✨ feat: 智乃扔，源石封印，你不懂啦

--- meme_generator/memes/spider/__init__.py
+++ meme_generator/memes/spider/__init__.py
@@ -1,59 +1,42 @@
+import random
 from datetime import datetime
 from pathlib import Path
 
+from PIL.Image import Image as IMG
 from pil_utils import BuildImage
 
-from meme_generator import MemeArgsModel, add_meme
-from meme_generator.exception import TextOverLength
+from meme_generator import add_meme
 from meme_generator.utils import save_gif
 
 img_dir = Path(__file__).parent / "images"
 
 
-def spider(
-    images: list[BuildImage], texts, args
-):
-    user_size = (75, 75)
-    user_locs = [
-        (178, 27),
-        (178, 27),
-        (168, 27),
-        (158, 27),
-        (148, 27),
-        (138, 27),
-        (133, 27),
-        (118, 27),
-        (110, 27),
-        (105, 27),
-        (95, 27),
-        (90, 27),
-        (78, 27),
-        (78, 27),
-        (78, 27),
-        (78, 27),
-        (90, 27),
-        (95, 27),
-        (105, 27),
-        (110, 27),
-        (119, 27),
-        (131, 27),
-        (136, 27),
-        (146, 27),
-        (156, 27),
-        (168, 27),
-        (176, 27),
-        (176, 27),
+def spider(images: list[BuildImage], texts, args):
+    head = images[0].convert("RGBA").circle().resize((80, 80))
+    # fmt: off
+    Xs = [
+        174, 174, 174, 169,
+        165, 160, 154, 150,
+        144, 141, 137, 133,
+        130, 119, 115, 113,
+        108, 103, 103, 97,
+        91, 85, 87, 79,
+        74, 79, 75, 75,
+        78, 79, 77, 77,
+        70, 81, 93, 94,
+        104, 110, 119, 123,
+        131, 134, 143, 154,
+        158, 161, 163, 169,
+        174, 173, 174, 173
     ]
-
-    frames = []
-    user = images[0].resize(user_size).circle()
-
-    for i in range(len(user_locs)):
+    # fmt: on
+    frames: list[IMG] = []
+    for i in range(52):
+        pos = (Xs[i], 24 + random.randint(-1, 1))
         frame = BuildImage.open(img_dir / f"{i}.png")
-        frame.paste(user, user_locs[i], alpha=True)
+        frame.paste(head, pos, alpha=True)
         frames.append(frame.image)
-
-    return save_gif(frames, 0.05)
+    return save_gif(frames, 0.04)
 
 
 add_meme(
@@ -61,7 +44,7 @@ add_meme(
     spider,
     min_images=1,
     max_images=1,
-    keywords=["蜘蛛"],
-    date_created=datetime(2025, 4, 26),
-    date_modified=datetime(2025, 4, 26),
+    keywords=["蜘蛛", "蜘蛛爬"],
+    date_created=datetime(2025, 4, 27),
+    date_modified=datetime(2025, 4, 27),
 )

61858692cc4aaf6768f46bc403af414917bdd5c3
02 <hentai02y@gmail.com>
2025-06-06 16:21:53 +0800

    ✨ feat: 守岸人举牌，撕衣服

--- meme_generator/memes/kurogames_the_shorekeeper_holdsign/__init__.py
+++ meme_generator/memes/kurogames_the_shorekeeper_holdsign/__init__.py
@@ -7,22 +7,21 @@ from meme_generator import add_meme
 from meme_generator.exception import TextOverLength
 from meme_generator.tags import MemeTags
 
-
 img_dir = Path(__file__).parent / "images"
 
 
-def kurogames_gugu_blowfish_small_classes(images, texts: list[str], args):
+def kurogames_the_shorekeeper_holdsign(images, texts: list[str], args):
     text = texts[0]
     frame = BuildImage.open(img_dir / "0.jpg")
     try:
         frame.draw_text(
-            (65, 173, 1301, 897),
+            (155, 455, 462, 698),
             text,
             fill=(0, 0, 0),
             allow_wrap=True,
-            max_fontsize=150,
-            min_fontsize=20,
-            lines_align="left",
+            max_fontsize=120,
+            min_fontsize=30,
+            lines_align="center",
             font_families=["FZShaoEr-M11S"],
         )
     except ValueError:
@@ -31,13 +30,13 @@ def kurogames_gugu_blowfish_small_classes(images, texts: list[str], args):
 
 
 add_meme(
-    "kurogames_gugu_blowfish_small_classes",
-    kurogames_gugu_blowfish_small_classes,
+    "kurogames_the_shorekeeper_holdsign",
+    kurogames_the_shorekeeper_holdsign,
     min_texts=1,
     max_texts=1,
-    default_texts=["松伦哥,不要再涩涩了"],
-    keywords=["咕咕河豚小课堂","小课堂"],
+    default_texts=["松伦狗策划太坏了"],
+    keywords=["守岸人举牌"],
     tags=MemeTags.wuthering_waves,
-    date_created=datetime(2025, 6, 1),
-    date_modified=datetime(2025, 6, 1),
+    date_created=datetime(2025, 6, 5),
+    date_modified=datetime(2025, 6, 5),
 )
